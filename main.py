d567d9bfdb3e25ddfb6ab3321cd49636af9aa793
dioriy <tdiyorbek58@gmail.com>
2025-06-08 12:49:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, request, send_file
 from datetime import datetime
 
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FILE = os.path.join(BASE_DIR, "log.txt")
+HTML_FILE = os.path.join(BASE_DIR, "index.html")
 
 @app.route("/log", methods=["POST"])
 def log():
@@ -12,30 +16,29 @@ def log():
     combined = f"{ip_address} | {user_agent}"
 
     try:
-        with open("log.txt", "r") as f:
+        with open(LOG_FILE, "r") as f:
             if combined in f.read():
                 return "Duplicate ignored"
     except:
         pass
 
-    with open("log.txt", "a") as f:
+    with open(LOG_FILE, "a") as f:
         f.write(f"{timestamp} | {combined}\n")
 
     return "Logged!"
 
 @app.route("/")
 def index():
-    return send_file("index.html")
+    return send_file(HTML_FILE)
 
 @app.route("/count")
 def count():
     try:
-        with open("log.txt", "r") as f:
+        with open(LOG_FILE, "r") as f:
             lines = f.readlines()
             return f"QR linkini {len(lines)} marta ochishgan ✅"
     except:
         return "0"
 
-# 🧠 Eng muhim qism:
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
